ce2aa9dcca0fc466deafc12788df173fd90b026b
Unknown <milosz23wow@gmail.com>
2019-01-22 13:30:58 +0100

    update configs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /** @format */
 
 import { AppRegistry } from "react-native";
-import App from "./App";
+import InitialConfigScreen from "./src/InitialConfigScreen";
 import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => InitialConfigScreen);
